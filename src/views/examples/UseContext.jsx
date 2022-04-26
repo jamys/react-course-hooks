b7da6331cc01e38d46b3fb2bf964a83013437b0b
Jamys Solsol <jamyssolsol@gmail.com>
2022-04-26 01:51:19 -0400

    Exercicio UseContext

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useContext, useEffect } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
+import DataContext from '../../data/DataContext';
+import { AppContext } from '../../data/Store';
 
 const UseContext = (props) => {
+    const context = useContext(DataContext);
+
+    function addNumber(e) {
+        context.setState({
+            ...context.state,
+            number: context.state.number + e
+        })
+    }
+
+    const { number, text, setNumber } = useContext(AppContext);
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+            <SectionTitle title="Execício #01" />
+            <div className="center">
+                <span className="text">{context.state.text}</span>
+                <span className="text">{context.state.number}</span>
+                <div>
+                    <button className="btn" onClick={() => addNumber(1)}>+1</button>
+                    <button className="btn" onClick={() => addNumber(-1)}>-1</button>
+                </div>
+            </div>
+            <SectionTitle title="Execício #01" />
+            <div className="center">
+                <span className="text">{text}</span>
+                <span className="text">{number}</span>
+                <div>
+                    <button className="btn" onClick={() => setNumber(number - 1)}>-1</button>
+                    <button className="btn" onClick={() => setNumber(number + 1)}>+1</button>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default UseContext
+export default UseContext;
